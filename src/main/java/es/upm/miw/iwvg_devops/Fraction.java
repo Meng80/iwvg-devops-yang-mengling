b5f47082f537bdfff8cd4d019e36cb0fcd088b59
Meng80 <metang@alumnos.upm.es>
2023-09-27 18:48:21 +0200

    enhanced Fraction #9

--- src/main/java/es/upm/miw/iwvg_devops/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/Fraction.java
@@ -33,6 +33,28 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean idProper() { return Math.abs(numerator) < Math.abs(denominator); }
+
+    public boolean isImproper() { return Math.abs(numerator) >= Math.abs(denominator); }
+
+    public boolean isEquivalent(Fraction other){return (numerator * other.denominator) == (denominator * other.numerator); }
+
+    public Fraction add(Fraction other){
+        int newNumerator = (numerator * other.denominator) + (other.numerator * denominator);
+        int newDenominator = denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction divide(Fraction other){
+        if (other.numerator == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        }
+        int newNumerator = numerator * other.denominator;
+        int newDenominator = denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+
     @Override
     public String toString() {
         return "Fraction{" +
